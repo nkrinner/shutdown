47ded4cbe43f23764d96510923df12343280a50a
Nanuk Krinner <nkrinner@suse.de>
2014-09-03 22:26:50 +0200

    Add documentation file

--- shutdown/shutdown_copy.sh
+++ shutdown/shutdown_copy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Just a helper to copy files on all nodes in a cluster if desired so
+
 for node in $(crowbar machines list); do
   scp $1 root@$node:~
   if [[ $? == "0" ]]; then
--- shutdown/shutdown_instances.sh
+++ shutdown/shutdown_instances.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Shutdown all instances in a cloud - untested, probably not fully working
+
+if [ -f "~/.openrc" ]
+  . ~/.openrc
+fi
+
 for tenant_id in $(keystone tenant-list | grep -v ' service ' | tail -n +4 | head -n -1 | cut -d'|' -f2); do
   for server_id in $(nova --os-tenant-id "$tenant_id" list | grep -v PAUSED | tail -n +4 | head -n -1 | cut -d'|' -f2); do
     echo -n "Pausing $server_id (tenant $tenant_id)..."
